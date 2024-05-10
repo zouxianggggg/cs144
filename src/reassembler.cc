46e66cc7fa342438d05a52fa1e324c71b76d5e6f
zouxiang <zouxianggggg@outlook.com>
2024-05-10 17:34:09 +0800

    理解错误，看了测试用例才发现需要一个字节一个字节的处理，目前是对一整个字符串进行处理。重新写

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -5,6 +5,76 @@ using namespace std;
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring )
 {
   // Your code here.
+  //首先判断当前的first_index，如果当前first_index正好是current_index+1，那么就可以将其插入到bytestream中
+  //如果index对不上的话，那么就暂时先将其放在缓冲区中
+  if (first_index == _currentIndex)   //正好是下一个index的情况
+  {
+    //首先要判断我能不能写进去，也就是bytestream的capacity够不够
+    uint64_t dataLength = data.size();
+    uint64_t ByteStreamCap = output_.writer().available_capacity();
+    if(dataLength > ByteStreamCap)
+    {
+      //如果大于，能写多少写多少，剩下的放进缓冲区？
+      return;
+    }
+
+
+
+    output_.writer().push(data);
+    //auto wt = this->writer();
+    //wt.push(data);
+    _currentIndex += dataLength;
+    //如果是laststring，需要关闭writer
+    if(is_last_substring)
+    {
+      output_.writer().close();
+      return;
+    }
+    else
+    {
+      //这个插完了，还要将map里的data都遍历一遍，看有没有可以插入的
+      while(recurseInsert()){};
+    }
+
+  }
+  else if(first_index > _currentIndex)
+  {
+    //大于的话，先暂存，然后等待时机push，存的时候，既要考虑index之间的关系，还要考虑当前data的长度是否超过了bytestream的当前可用容量
+    std::pair<std::string,bool> currentStr = {data,is_last_substring};
+    uint64_t last_index = first_index + data.size();
+    //既然是每一个byte一个index，那么还要计算一下他的index的上下界
+    bool insertFlag = true;
+    uint64_t currentInternalBufferSize = 0;
+    for(auto &mapPair : _internalBuffer)
+    {
+      currentInternalBufferSize += mapPair.second.first.size();
+      //当前substring的第一个byte的index
+      uint64_t curSubstringStartIndex = mapPair.first;
+      uint64_t curSubstringEndIndex = mapPair.second.first.size()+ curSubstringStartIndex;
+      //如果当前的data和mapPair的data的index之间有重叠的话，就不能暂存
+      bool isoverlap = ((first_index <= curSubstringEndIndex)&&(curSubstringStartIndex <= last_index));
+      if(isoverlap)
+      {
+        insertFlag = false;
+      }
+    }
+    //接下来是检查internalbuffer中所有的data加起来的长度是否超过了bytestream的可用长度
+    uint64_t curByteStreamCapacity = this->writer().available_capacity();
+    if((currentInternalBufferSize + data.size()) > curByteStreamCapacity)
+    {
+      insertFlag = false;
+    }
+    if(insertFlag)
+    {
+      //都没问题了就放进internalBuffer中去
+      _internalBuffer.emplace(first_index,currentStr);
+    }
+    
+
+
+  }
+  
+
   (void)first_index;
   (void)data;
   (void)is_last_substring;
@@ -13,5 +83,45 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
 uint64_t Reassembler::bytes_pending() const
 {
   // Your code here.
-  return {};
+  uint64_t ret = 0;
+  for(auto &map:_internalBuffer)
+  {
+    ret += map.second.first.size();
+  }
+  return ret;
+}
+
+bool Reassembler::recurseInsert() {
+  //在这里进行递归的插入操作
+
+  //首先停止条件，有两种停止条件，_internalBuffer为空或者没有index符合要求的数据
+  for(auto &subs: _internalBuffer)
+  {
+    if(subs.first == _currentIndex)
+    {
+
+      //capacity需要大于datalength才能插入
+
+
+
+
+      //开始插入
+      uint64_t datalength = subs.second.first.size();
+      
+      output_.writer().push(subs.second.first);
+      _currentIndex += datalength;
+      //如果是结束符号，需要关闭writer
+      if(subs.second.second)
+      {
+        output_.writer().close();
+        return false;
+      }
+      else{
+        _internalBuffer.erase(subs.first);
+        return true;
+      }
+      //插入完并且不算结束之后把当前插入的从buffer中删掉
+    }
+  }
+  return false;
 }
--- src/reassembler.hh
+++ src/reassembler.hh
@@ -1,12 +1,13 @@
 #pragma once
 
 #include "byte_stream.hh"
-
+#include <unordered_map>
+#include <utility>
 class Reassembler
 {
 public:
   // Construct Reassembler to write into given ByteStream.
-  explicit Reassembler( ByteStream&& output ) : output_( std::move( output ) ) {}
+  explicit Reassembler( ByteStream&& output ) : output_( std::move( output ) ) ,_currentIndex(0),_capacity(1024),_internalBuffer(){}
 
   /*
    * Insert a new substring to be reassembled into a ByteStream.
@@ -41,5 +42,12 @@ public:
   const Writer& writer() const { return output_.writer(); }
 
 private:
+  bool recurseInsert();
+
   ByteStream output_; // the Reassembler writes to this ByteStream
+
+  //我的想法是用一个map来作为reassembler的缓冲区，不需要考虑index重复的情况，用index来作为
+  uint64_t _currentIndex;
+  uint64_t _capacity;
+  std::unordered_map<uint64_t,std::pair<std::string,bool>> _internalBuffer;
 };
