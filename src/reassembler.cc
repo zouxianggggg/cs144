c3d06d637e1390c4c85ba12113d60b5ad9263483
zouxianggggg <zouxianggggg@outlook.com>
2024-05-14 09:10:45 +0000

    lab1改了一版，等回家测试一下

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -1,4 +1,6 @@
 #include "reassembler.hh"
+#include <bits/stdint-uintn.h>
+#include <cstdint>
 
 using namespace std;
 
@@ -13,96 +15,70 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
     uint64_t writeIndex = 0;
     uint64_t dataLength = data.size();
     uint64_t ByteStreamCap = output_.writer().available_capacity();
-    while ((writeIndex<dataLength)&&ByteStreamCap)
+    while ((writeIndex < dataLength) && ByteStreamCap)
     {
-     output_.writer().push(std::string(data.at(writeIndex++))); 
-     _currentIndex++;
-     ByteStreamCap = output_.writer().available_capacity();
+      std::string singleChar(1, data.at(writeIndex++));
+      output_.writer().push(singleChar);
+      _currentIndex++;
+      ByteStreamCap = output_.writer().available_capacity();
     }
-    //如果capacity为0，则说明不能再写了，此时也不会将剩下的byte放入buffer中，而是直接丢弃
-    //如果是data写完了且正好是last，需要关闭writer
-    if((writeIndex == dataLength) && is_last_substring)
+    // If capacity is 0, it means we can't write anymore. In this case, we don't put the remaining bytes into the buffer and simply discard them.
+    // If data is fully written and it is the last substring, we need to close the writer.
+    if ((writeIndex == dataLength) && is_last_substring)
     {
       output_.writer().close();
       return;
     }
     //如果不是终结符，并且当前已经插入完毕的话，继续递归的插入
-    
-
-
-    output_.writer().push(data);
-    //auto wt = this->writer();
-    //wt.push(data);
-    _currentIndex += dataLength;
-    //如果是laststring，需要关闭writer
-    if(is_last_substring)
-    {
-      output_.writer().close();
-      return;
-    }
-    else
-    {
-      //这个插完了，还要将map里的data都遍历一遍，看有没有可以插入的
-      while(recurseInsert()){};
-    }
-
+    while (recurseInsert()) {}
   }
   else if(first_index > _currentIndex)
   {
-    //大于的话，先暂存，然后等待时机push，存的时候，既要考虑index之间的关系，还要考虑当前data的长度是否超过了bytestream的当前可用容量
-    std::pair<std::string,bool> currentStr = {data,is_last_substring};
-    uint64_t last_index = first_index + data.size();
-    //既然是每一个byte一个index，那么还要计算一下他的index的上下界
-    bool insertFlag = true;
-    uint64_t currentInternalBufferSize = 0;
-    for(auto &mapPair : _internalBuffer)
+    //大于的话，先暂存
+    //需不需要考虑capacity的大小？需要的，跟上面的直接插入一样，也是暂存datasize和capacity的最小值
+    uint64_t datasize = data.size();
+    uint64_t ByteStreamCap = output_.writer().available_capacity();
+    uint64_t curBufIndex = 0;
+    while ((curBufIndex < datasize) && ByteStreamCap)
     {
-      currentInternalBufferSize += mapPair.second.first.size();
-      //当前substring的第一个byte的index
-      uint64_t curSubstringStartIndex = mapPair.first;
-      uint64_t curSubstringEndIndex = mapPair.second.first.size()+ curSubstringStartIndex;
-      //如果当前的data和mapPair的data的index之间有重叠的话，就不能暂存
-      bool isoverlap = ((first_index <= curSubstringEndIndex)&&(curSubstringStartIndex <= last_index));
-      if(isoverlap)
-      {
-        insertFlag = false;
+      //判断当前index如果是data的最后一个byte，而且如果是终结符，那么要将对应的bool值变为true
+      if(curBufIndex == datasize - 1){
+        if(is_last_substring)
+        {
+          _internalBuffer.emplace(first_index+curBufIndex,std::make_pair(std::string(1,data.at(curBufIndex)),true));
+        }
       }
+      
+      _internalBuffer.emplace(first_index+curBufIndex,std::make_pair(std::string(1,data.at(curBufIndex)),false));
+
     }
-    //接下来是检查internalbuffer中所有的data加起来的长度是否超过了bytestream的可用长度
-    uint64_t curByteStreamCapacity = this->writer().available_capacity();
-    if((currentInternalBufferSize + data.size()) > curByteStreamCapacity)
-    {
-      insertFlag = false;
-    }
-    if(insertFlag)
-    {
-      //都没问题了就放进internalBuffer中去
-      _internalBuffer.emplace(first_index,currentStr);
-    }
+
+
     
 
 
-  }
-  
 
-  (void)first_index;
-  (void)data;
-  (void)is_last_substring;
+
+
+  }
+  else
+  {
+    //小于的话，直接丢弃
+  }
 }
 
 uint64_t Reassembler::bytes_pending() const
 {
   // Your code here.
-  uint64_t ret = 0;
-  for(auto &map:_internalBuffer)
-  {
-    ret += map.second.first.size();
-  }
-  return ret;
+  return _internalBuffer.size();
 }
 
 bool Reassembler::recurseInsert() {
   //在这里进行递归的插入操作
+  if(output_.writer().available_capacity() == 0){
+    return false;
+  }
+
 
   //首先停止条件，有两种停止条件，_internalBuffer为空或者没有index符合要求的数据
   for(auto &subs: _internalBuffer)
@@ -111,15 +87,10 @@ bool Reassembler::recurseInsert() {
     {
 
       //capacity需要大于datalength才能插入
-
-
-
-
       //开始插入
-      uint64_t datalength = subs.second.first.size();
       
-      output_.writer().push(subs.second.first);
-      _currentIndex += datalength;
+      output_.writer().push(std::string(1,subs.second.first));
+      _currentIndex ++;
       //如果是结束符号，需要关闭writer
       if(subs.second.second)
       {
